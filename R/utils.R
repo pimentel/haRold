160a350240c8aca505d3f9b9d67ef2acb370d462
Harold Pimentel <haroldpimentel@gmail.com>
2014-12-29 19:15:45 -0800

    outer join

--- R/utils.R
+++ R/utils.R
@@ -7,7 +7,7 @@
 #' @param simplify if true, return a matrix of the list
 #' @return a list with the pair sequence
 #' @export
-#' @examples 
+#' @examples
 #' adjacentPairs(3, 10)
 #' adjacentPairs(0, 4)
 #' t(adjacentPairs(0, 4, TRUE))
@@ -18,4 +18,22 @@ adjacentPairs <- function(start, stop, simplify = FALSE)
     first <- start:(stop-1)
     second <- (start+1):stop
     mapply(c, first, second, SIMPLIFY = simplify)
+}
+
+#' Perform a full outer join
+#'
+#' Peform a full outer join using dplyr \code{\link{dplyr::left_join}}. For
+#' additional documentation, see \code{\link{dplyr::left_join}}.
+#'
+#' @param x the first \code{data.frame}
+#' @param y the second \code{data.frame}
+#' @param by the columns to do the join by.
+#' @return a \code{data.frame} with the full outer join
+#' @export
+outer_join <- function(left, right, by = NULL, ...)
+{
+    ldf <- dplyr::left_join(df1, right, by = by, copy = FALSE, ...)
+    rdf <- dplyr::left_join(df2, left, by = by, copy = FALSE, ...)
+
+    dplyr::union(ldf, rdf)
 }
